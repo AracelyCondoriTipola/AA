f47e4a806b19ed0798dafad59e950d9e7dfd5b3e
AracelyCondoriTipola <aracely96.ac>
2022-05-23 19:01:12 -0400

    modificando rol controller

--- projectBackend/src/main/java/backend/project/controllers/RolController.java
+++ projectBackend/src/main/java/backend/project/controllers/RolController.java
@@ -33,4 +33,44 @@ import backend.project.service.RolService;
 @CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/rol")
 
+public class RolController {
+    @Autowired
+    private RolService rolService;
+
+    @Operation(summary = "Get a rol by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the rol",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Rol.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Rol not found",
+                    content = @Content) })
+
+    //	OBTENER ROL POR ID
+    @GetMapping("/{id}")
+    public Optional<Rol> getById (@Parameter(description = "id of rol to be searched")
+                                  @PathVariable Integer id) {
+
+        return (Optional<Rol>) rolService.getById(id);
+    }
+
+
+    @Operation(summary = "Get rol list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the rol",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Rol.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Rol not found",
+                    content = @Content) })
+    //	OBTENER TODAS LOS ROLES
+    @GetMapping
+    public List<Rol> ListarTodos() {
+
+        return rolService.BuscarTodos();
+    }
+
+}
 
