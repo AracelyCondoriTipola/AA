0879f9bc55b0ddac3fa302889059bffc65e08d63
nina mayumi <mayunina55@gmail.com>
2022-05-25 10:39:42 -0400

    modificando ClientDto

--- projectBackend/src/main/java/backend/project/dto/ClientDto.java
+++ projectBackend/src/main/java/backend/project/dto/ClientDto.java
@@ -22,6 +22,9 @@ public class ClientDto {
     public String getUsername() {
         return username;
     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     @Override
     public String toString() {
