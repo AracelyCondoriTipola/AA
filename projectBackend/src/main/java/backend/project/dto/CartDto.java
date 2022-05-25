fc65a8e00f8416f4cf69c8b85ec11e2b66e3d6f4
nina mayumi <mayunina55@gmail.com>
2022-05-25 10:25:38 -0400

    modificando CartDto

--- projectBackend/src/main/java/backend/project/dto/CartDto.java
+++ projectBackend/src/main/java/backend/project/dto/CartDto.java
@@ -19,7 +19,9 @@ public class CartDto {
     public String getName() {
         return name;
     }
-
+    public void setName(String name) {
+        this.name = name;
+    }
     @Override
     public String toString() {
         return "CartDto{" +
